88af6b30b82fb65f2adab47589d791e7a478cbef
dongchuni <karmadc070@gmail.com>
2026-07-22 20:09:48 +0900

    외부 공유: trycloudflare 터널용 allowedDevOrigins 설정

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  // 외부 공유(트라이클라우드플레어 터널)에서 dev 자원 요청을 허용
+  allowedDevOrigins: ["*.trycloudflare.com"],
 };
 
 export default nextConfig;
